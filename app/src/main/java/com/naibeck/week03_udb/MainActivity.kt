c1d8a6ae31882b0ea89afdb659463d709f42e7e2
Kevin <kg-diseno@outlook.es>
2020-08-14 16:06:48 -0300

    Step 5: Link Button to Activity and make a simple click interaction

--- app/src/main/java/com/naibeck/week03_udb/MainActivity.kt
+++ app/src/main/java/com/naibeck/week03_udb/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.naibeck.week03_udb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val payButton = findViewById<Button>(R.id.payButton)
+        payButton.setOnClickListener {
+            payButton.text = changeButtonText(payButton)
+            Toast.makeText(this, "This will move to my next activity", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun changeButtonText(payButton: Button): String {
+        return if (payButton.text.contains("Pagar", ignoreCase = true)) {
+            "Pagando..."
+        } else {
+            getString(R.string.pay_action)
+        }
     }
 }
---
+++ app/src/main/java/com/naibeck/week03_udb/PaymentFormActivity.kt
@@ -0,0 +1,11 @@
+package com.naibeck.week03_udb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaymentFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_form)
+    }
+}
