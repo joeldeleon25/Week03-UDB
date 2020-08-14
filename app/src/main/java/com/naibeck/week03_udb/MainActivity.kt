7f4cb909d22913e728f00dc4f257952ae6ebf269
Kevin Gomez <kevin.gomez@overactive.com>
2020-08-14 14:40:04 -0300

    Base project

---
+++ app/src/main/java/com/naibeck/week03_udb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.naibeck.week03_udb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
