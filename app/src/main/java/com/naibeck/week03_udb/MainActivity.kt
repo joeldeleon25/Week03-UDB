14372dc16b8fe85a173fc74009ea1dacd7005392
Kevin <kg-diseno@outlook.es>
2020-08-14 16:47:15 -0300

    Step 8: Add Timber and track activity lifecycle

--- app/src/main/java/com/naibeck/week03_udb/MainActivity.kt
+++ app/src/main/java/com/naibeck/week03_udb/MainActivity.kt
@@ -5,15 +5,41 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.naibeck.week03_udb.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Timber.d("Activity created")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         binding.payButton.setOnClickListener {
             val intent = Intent(this, PaymentFormActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.d("Activity started")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.d("Activity resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.d("Activity paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.d("Activity stopped")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.d("Activity destroyed")
     }
 }
--- app/src/main/java/com/naibeck/week03_udb/WeekApplication.kt
+++ app/src/main/java/com/naibeck/week03_udb/WeekApplication.kt
@@ -1,2 +1,15 @@
 package com.naibeck.week03_udb
 
+import android.app.Application
+import timber.log.Timber
+
+class WeekApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
